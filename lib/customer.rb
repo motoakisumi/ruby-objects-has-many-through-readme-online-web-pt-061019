7061af30919216f0cbf66d9dcceb859565e04368
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-12 05:42:55 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
-  
+  # ./lib/customer.rb
+class Customer
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
 end
